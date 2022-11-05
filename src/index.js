7a50cd039805f6d2c9da697492430e7cef226255
memset0 <34177126+memset0@users.noreply.github.com>
2022-11-05 19:12:39 +0800

    feat: use yaml format data files as default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const YAML = require('yaml');
 
 const parse = require('./parser');
 
@@ -9,13 +10,15 @@ const projectRoot = path.join(__dirname, '../');
 
 
 function run() {
-	const data = JSON.parse(fs.readFileSync(path.join(projectRoot, 'data.json')).toString() || '{}');
-	const cache = JSON.parse(fs.readFileSync(path.join(projectRoot, 'cache.json')).toString() || '{}');
 	// const css = fs.readFileSync(path.join(projectRoot, 'style.css')).toString();
 	const css = '';
+	const config = { css }
 
-	const response = parse(data, cache, { css });
-	fs.writeFileSync(path.join(projectRoot, 'cache.json'), JSON.stringify(response.cache, null, 2));
+	const data = JSON.parse(fs.readFileSync(path.join(projectRoot, 'data.json')).toString() || '{}');
+	const cache = YAML.parse(fs.readFileSync(path.join(projectRoot, 'cache.yaml')).toString() || '{}');
+
+	const response = parse(data, cache, config);
+	fs.writeFileSync(path.join(projectRoot, 'cache.yaml'), YAML.stringify(response.cache, null, 2));
 	fs.writeFileSync(path.join(projectRoot, 'index.html'), response.html);
 }
 
